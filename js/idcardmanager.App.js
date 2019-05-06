981882ec6f6c54bb497222682e6ddd003241879c
IPA <IPA@NB-019-000>
2019-05-06 09:08:02 +0200

    created main gui

--- js/idcardmanager.App.js
+++ js/idcardmanager.App.js
@@ -1,7 +1,208 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+/* global kijs, idcardmanager */
 
+idcardmanager.App = class idcardmanager_App { 
+
+    constructor(config={}) {
+
+            this._userDataView = null;
+            this._loginWindow = null;
+            this._viewport = null;
+
+            // RPC-Instanz
+            var rpcConfig = {};
+            if (config.ajaxUrl) {
+                rpcConfig.url = config.ajaxUrl;
+            }
+            this._rpc = new kijs.gui.Rpc(rpcConfig);
+    }
+    
+    // --------------------------------------------------------------
+    // MEMBERS
+    // --------------------------------------------------------------
+    
+    runApp() {
+        this._userDataView = new idcardmanager.UserDataView({
+            rpc: this._rpc
+        });
+        
+        const mainPanel = this.createMainPanel();
+
+        // ViewPort erstellen
+        this._viewport = new kijs.gui.ViewPort({
+            cls: 'kijs-flexcolumn',
+            elements:[
+                mainPanel
+            ]
+        });
+        
+        // feststellen ob ein Benutzer angemeldet ist
+        this._rpc.do('idcardmanager.checkLogin', null, 
+        function(response) {
+            if (response.username !== false) {
+                sessionStorage.setItem('Benutzer', response.data.username);
+
+                // Caption des Logout-Buttons setzen
+                this._viewport.render();
+                let caption = 'angemeldet als ' + sessionStorage.getItem('Benutzer') + '&nbsp;';
+                mainPanel.headerBar.down('btnLogout').caption = caption;
+            } else {
+                this.showLoginWindow();
+            }
+        }, this, false, this._viewport, 'dom', false);
+    }
+    
+    createMainPanel() {
+        return new kijs.gui.Panel({
+            name: 'mainPanel',
+            caption: 'IDCardManager',
+            footerCaption: '&copy; 2019 by Nicolas Burgunder',
+            cls: 'kijs-flexrow',
+            style:{
+                flex: 1
+            },
+            headerBarElements:{
+                xtype: 'kijs.gui.Button',
+                name: 'btnLogout',
+                iconChar: '&#xf011',
+                toolTip: 'ausloggen',
+                style:{
+                    marginRight: '6px',
+                    display: 'flex',
+                    flexDirection: 'row-reverse'
+                },
+                on:{
+                    click: this._onBtnLogoutClick,
+                    context: this
+                }
+            },
+            elements:[                
+            {
+                xtype: 'kijs.gui.Container',
+                cls: 'kijs-flexrow',
+                style: {
+                    flex: 1
+                },
+                elements: [
+                    // LEFT
+                    {
+                        xtype: 'kijs.gui.Panel',
+                        caption: 'Suche',
+                        collapsible: 'left',
+                        width: 240,
+                        defaults:{
+                            style:{
+                                margin: '10px'
+                            }
+                        },                       
+                        elements:[
+                            {
+                                xtype: 'kijs.gui.field.Text',
+                                name: 'name',
+                                label: 'Name',
+                                labelWidth: 80
+                            },{
+                                xtype: 'kijs.gui.field.Text',
+                                name: 'firstName',
+                                label: 'Vorname',
+                                labelWidth: 80
+                            },{
+                                xtype: 'kijs.gui.field.Number',
+                                name: 'employeeId',
+                                label: 'Mitarbeiternr.',
+                                labelWidth: 80,
+                                minValue: 0
+                            },{
+                                xtype: 'kijs.gui.field.DateTime',
+                                name: 'valid',
+                                label: 'Gültigkeitsdatum',
+                                labelWidth: 80,
+                                hasTime: false
+                            },{
+                                xtype: 'kijs.gui.Icon',
+                                cls: 'help-icon',
+                                height: 18,
+                                width: 18,
+                                iconChar: '&#xf059',
+                                toolTip: 'Mindestens ein Feld muss ausgefüllt werden.'
+                                        +'<br />Das Zeichen &#x2731; kann als Wildcard eingesetzt werden.',
+                                style:{
+                                    margin: '0 10px 0 0'
+                                }
+                            },{
+                                xtype: 'kijs.gui.Button',
+                                name: 'btnSearch',
+                                isDefault: true,
+                                width: 100,
+                                height: 30,
+                                caption: 'Suchen',
+                                on:{
+                                    click: this._onBtnSearchClick,
+                                    context: this
+                                },
+                                style:{
+                                    margin: '0 10px 10px 10px'
+                                }
+                            }
+                        ]
+                    },{
+                        xtype: 'kijs.gui.Splitter',
+                        targetPos: 'left'
+                    },{
+                        xtype: 'kijs.gui.Panel',
+                        caption: 'Ergebnisse',
+                        style: {
+                            flex: 1
+                        },
+                        innerStyle: {
+                            overflowY: 'auto'
+                        },
+                        headerElements:[
+                            {
+                                xtype: 'kijs.gui.Button',
+                                iconChar: '&#xf02f',
+                                toolTip: 'Druckvorlage erstellen',
+                                on:{
+                                    click: this._onBtnPrintClick,
+                                    context: this
+                                }
+                            }
+                        ],
+                        elements:[
+                            //this._userDataView 
+                        ]
+                    }
+                ]
+            }]
+        });
+    }
+    
+    showLoginWindow() {
+        this._loginWindow = new idcardmanager.LoginWindow({
+            rpc: this._rpc,
+            facadeFnSave: 'idcardmanager.loginUser',
+            on:{
+                afterSave: this._onLoginWindowAfterSave,
+                context: this
+            }
+        });
+        this._loginWindow.show();
+    }
+    
+    //LISTENERS
+    _onBtnLogoutClick() {
+        
+    }
+    
+    _onBtnPrintClick() {
+        
+    }
+    
+    _onBtnSearchClick() {
+        
+    }
+    
+    _onLoginWindowAfterSave() {
+        
+    }
+};
