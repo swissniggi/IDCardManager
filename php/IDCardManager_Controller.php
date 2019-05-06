c6f9a72006416605e2f7d69f7ca0b5d5d8145472
IPA <IPA@NB-019-000>
2019-05-06 08:31:33 +0200

    - prepared functions - implemented login

--- php/IDCardManager_Controller.php
+++ php/IDCardManager_Controller.php
@@ -44,6 +44,34 @@ class IDCardManager_Controller {
                         case 'idcardmanager.checkLogin':
                             $objectResponse->responseData = ['username' => $this->sUsername ?? false];
                             break;
+                        
+                        case 'idcardmanager.loadEditorData':
+                            break;
+                        
+                        case 'idcardmanager.loginUser':
+                            $objectResponse->data = new stdClass();
+                            $arrayReturn = $this->_loginUser($objectRequest->requestData->formData);
+                            if ($arrayReturn instanceof Exception || $arrayReturn instanceof Error) {
+                                $this->_writeLog($arrayReturn->getMessage());
+                                if ($arrayReturn->getCode() === 123) {
+                                    $objectResponse->errorMsg = $arrayReturn->getMessage();
+                                } else {
+                                    $objectResponse->errorMsg = 'Anmeldung fehlgeschlagen.';
+                                }
+                            } else {
+                                $objectResponse->responseData = array(
+                                    'success' => 'true'
+                                );
+                                // Benutzername in Session speichern
+                                $_SESSION['username'] = $arrayReturn['username'];
+                            }
+                            break;
+                        
+                        case 'idcardmanager.searchUsers':
+                            break;
+                        
+                        case 'idcardmanager.updateUserData';
+                            break;
                     }
                 } catch (Exception $ex) {
                     $this->writeLog($ex->getMessage());
@@ -66,7 +94,82 @@ class IDCardManager_Controller {
     // --------------------------------------------------------------
     // PRIVATE MENBERS
     // --------------------------------------------------------------
-    private function _loginUser($objectUserData) {
+    private function _getEmployeeId($arrayUserInfo, $intIndex) {
+        // Ausweisnummer auslesen
+        if (array_key_exists('employeeid', $arrayUserInfo[$intIndex])) {
+            $sEmployeeId = $arrayUserInfo[$intIndex]['employeeid'][0];
+        } else {
+            $sEmployeeId = '--';
+        }
+        return utf8_encode($sEmployeeId);
+    }
+    
+    private function _getFirstName($arrayUserInfo, $intIndex) {
+        // Vorname auslesen
+        if (array_key_exists('givenname', $arrayUserInfo[$intIndex])) {
+            $sGivenName = $arrayUserInfo[$intIndex]['givenname'][0];
+        } else {
+            $sGivenName = '--';
+        }
+        return utf8_encode($sGivenName);
+    }
+    
+    private function _getImgPath($arrayUserInfo, $arrayFilter, $intIndex) {
         
+    }
+    
+    private function _getLastName($arrayUserInfo, $intIndex) {
+        if (array_key_exists('sn', $arrayUserInfo[$intIndex])) {
+            $sLastName = $arrayUserInfo[$intIndex]['sn'][0];
+        } else {
+            $sLastName = '--';
+        }
+        return utf8_encode($sLastName);
+    }
+    
+    private function _loginUser($arrayUserData) {
+        try{
+            $sUsername = mb_strtolower($arrayUserData->username);
+            $sPassword = $arrayUserData->password;
+
+            $con = ldap_connect($this->arrayLdap->ldapConnection);
+            $arrayConParts = explode('.',$this->arrayLdap->ldapConnection);
+            ldap_bind($con, $arrayConParts[1]."\\".$sUsername, $sPassword);
+
+            if ($this->arrayLdap->groupDn !== '') {
+                $sGroupDn = $this->arrayLdap->groupDn;
+            } else {
+                $sGroupDn = $this->arrayLdap->dn;
+            }
+
+            $arrayGroupSearchResult = ldap_search(
+                    $con,
+                    $sGroupDn,
+                    utf8_encode($this->arrayLdap->group)
+                    );
+            $arrayGroupInfo = ldap_get_entries($con, $arrayGroupSearchResult);
+
+            // vollständigen Namen ermitteln
+            $arrayNameSearchResult = ldap_search($con, $this->arrayLdap->dn, '(smaaccoutname='.$sUsername.')');
+            $arrayUserInfo = ldap_get_entries($con, $arrayNameSearchResult);
+            $sCommonName = $arrayUserInfo[0]['cn'][0];
+            
+            $boolIsMember = false;
+            foreach ($arrayGroupInfo[0]['member'] as $sMember) {
+                if (mb_strpos($sMember, $sCommonName) !== false) {
+                    $boolIsMember = true;
+                    break;
+                }
+            }
+            
+            if (!$boolIsMember) {
+                throw new Exception ('Zugriff verweigert!');
+            }
+            
+            $arrayReturn = array('username' => $sUsername);
+        } catch (Throwable $ex) {
+            $arrayReturn = $ex;
+        }
+        return $arrayReturn;
     }
 }
