8a5aa46b3927c82c3b56512c2b759fdf4f4fb4fa
IPA <IPA@NB-019-000>
2019-05-07 11:04:19 +0200

    removed 'this'

--- js/idcardmanager.App.js
+++ js/idcardmanager.App.js
@@ -250,7 +250,7 @@ idcardmanager.App = class idcardmanager_App {
             'firstName': this._userDataView.current._dataRow['firstName'],
             'employeeId': ''
         };
-        this._editorWindow._formPanel.load(data);
+        editorWindow._formPanel.load(data);
     }
     
     _onLoginWindowAfterSave() {
