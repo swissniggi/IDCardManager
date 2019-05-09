2dba04ec1572d07e415859a97dc0796e8ac0bc40
IPA <IPA@NB-019-000>
2019-05-09 13:28:24 +0200

    removed unused code

--- js/idcardmanager.EditorWindow.js
+++ js/idcardmanager.EditorWindow.js
@@ -149,9 +149,6 @@ idcardmanager.EditorWindow = class idcardmanager_EditorWindow extends kijs.gui.W
         let sEmployeeId = this._formPanel.down('employeeId').value;
         let sValidDate = this._formPanel.down('valid').value;
         
-        let dateNow = new Date();
-        dateNow.setHours(0,0,0,0);
-        
         if (sValidDate !== '' && !sValidDate) {
             kijs.gui.MsgBox.error('Fehler!','Gültigkeitsdatum hat falsches Format!');
             return false;
