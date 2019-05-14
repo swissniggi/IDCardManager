a22c84bcf6375f866445e549da39bfe92aee7a59
IPA <IPA@NB-019-000>
2019-05-14 14:14:23 +0200

    removed unneeded session_start()

--- php/IDCardManager_ImageManipulator.php
+++ php/IDCardManager_ImageManipulator.php
@@ -12,7 +12,6 @@ class IDCardManager_ImageManipulator {
      * @param string $sImgString
      */
     public static function saveImage($sLastName, $sFirstName, $sImgString) {
-        session_start();
         $img = imagecreatefromstring($sImgString);                       
         imagejpeg($img, 'userImages/'.session_id().'/'.$sLastName.'_'.$sFirstName.'.jpg');
         imagedestroy($img);
@@ -23,7 +22,6 @@ class IDCardManager_ImageManipulator {
      * alle Bilder im temporären Ordner löschen
      */
     public static function deleteAllImages() {
-        session_start();
         $arrayAllFiles = scandir('userImages/'.session_id());
         $arrayImages = array_diff($arrayAllFiles, array('.', '..'));
         
