42d6ed893871322f20bfb0a9247be4d6b64ed664
IPA <IPA@NB-019-000>
2019-05-14 09:12:15 +0200

    optimized error handling

--- php/IDCardManager_Controller.php
+++ php/IDCardManager_Controller.php
@@ -73,7 +73,7 @@ class IDCardManager_Controller {
                                 if ($arrayReturn->getCode() === 123) {
                                     $objectResponse->errorMsg = $arrayReturn->getMessage();
                                 } else {
-                                    $objectResponse->errorMsg = 'Anmeldung fehlgeschlagen. '.$arrayReturn->getMessage();
+                                    $objectResponse->errorMsg = 'Anmeldung fehlgeschlagen: Passwort oder Benutzername falsch!';
                                 }
                             } else {
                                 $objectResponse->responseData = array(
